831e483dd1f9522b580e5bc1e7c6c491f3e2aaeb
paledega <38860643+paledega@users.noreply.github.com>
2022-10-06 20:20:20 +0000

    Update alpine.sh

--- src/alpine.sh
+++ src/alpine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-DESTDIR=$(realpath ~/)
+DESTDIR=$(realpath ~/.var/app/io.github.paledega.alpine-rootfs/rootfs/)
 REPO=http://dl-cdn.alpinelinux.org/alpine/edge
 if [[ ! -f ${DESTDIR}/etc/os-release ]] ; then
     mkdir ${DESTDIR}/{dev,sys,proc} -p
@@ -12,10 +12,9 @@ if [[ ! -f ${DESTDIR}/etc/os-release ]] ; then
     rm -rf chroot
     cat /etc/resolv.conf > ${DESTDIR}/etc/resolv.conf
     echo "alpine" > ${DESTDIR}/etc/hostname
-    install ${DESTDIR}/usr/bin/proot.static ${DESTDIR}/proot
+    install ${DESTDIR}/usr/bin/proot.static ~/proot
 fi
-flatpak-spawn --host xhost + local: &>/dev/null || true
-exec ${DESTDIR}/proot -r ${DESTDIR}/ -w / -0 /bin/sh -c "
+exec ~/proot -r ${DESTDIR}/ -w / -0 /bin/sh -c "
             export PULSE_SERVER=127.0.0.1
             export USER=root
             export DISPLAY=:0
