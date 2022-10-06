355eaae17a78ff66a1e25565b27d5afb2aabe5b4
paledega <38860643+paledega@users.noreply.github.com>
2022-10-06 20:14:42 +0000

    Update alpine.sh

--- src/alpine.sh
+++ src/alpine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-DESTDIR=$(realpath ~/.alpine/)
+DESTDIR=$(realpath ~/)
 REPO=http://dl-cdn.alpinelinux.org/alpine/edge
 if [[ ! -f ${DESTDIR}/etc/os-release ]] ; then
     mkdir ${DESTDIR}/{dev,sys,proc} -p
@@ -15,23 +15,9 @@ if [[ ! -f ${DESTDIR}/etc/os-release ]] ; then
     install ${DESTDIR}/usr/bin/proot.static ${DESTDIR}/proot
 fi
 flatpak-spawn --host xhost + local: &>/dev/null || true
-bash
-exec flatpak-spawn --host --clear-env bwrap --bind ${DESTDIR}/ / \
-        --bind / /host \
-        --proc /proc \
-        --bind /sys /sys \
-        --dev /dev \
-        --bind /tmp /tmp \
-        --share-net \
-        --die-with-parent \
-        --hostname alpine \
-        --dir /run/user/ \
-        --unshare-uts \
-        --dev-bind /dev/dri /dev/dri \
-        /proot -w / -0 -r / /bin/sh -c "
+exec ${DESTDIR}/proot -r ${DESTDIR}/ -w / -0 /bin/sh -c "
             export PULSE_SERVER=127.0.0.1
             export USER=root
             export DISPLAY=:0
             . /etc/profile
-            exec /bin/bash
-        "
+            exec /bin/ash $@"
